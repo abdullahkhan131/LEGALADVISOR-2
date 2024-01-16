da51cb46d10b08b9596a78e80811f5ee189fd16e
abdullahkhan131 <96387086+abdullahkhan131@users.noreply.github.com>
2024-01-16 16:22:41 +0500

    Update final_streamlit_program_.py

--- final_streamlit_program_.py
+++ final_streamlit_program_.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1qXHTmh_LYiOdPR8PP9samEk39KnK54_R
 """
 
-!pip install langchain
+#
 import langchain
 import os
 import openai
